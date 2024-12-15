97ffbacb1e794dd3d5c2cf618b2ce9bc7e87097b
Xavier Franquet <xavier@franquet.es>
2024-12-15 16:08:47 +0100

    Add day14 placeholder

---
+++ src/Application/Day14.go
@@ -0,0 +1,20 @@
+package Application
+
+import (
+	"bufio"
+)
+
+type Day14 struct {
+}
+
+func (d *Day14) Part1(input *bufio.Scanner) error {
+
+	return nil
+}
+
+func (d *Day14) Part2(input *bufio.Scanner) error {
+	return nil
+}
+
+func (d *Day14) parseInput(input *bufio.Scanner) {
+}
